7848c8c0f5cde47c0e6f6772638bdc1743bfe9d8
Hayyan Khan <Hayyank2425@gmail.com>
2024-03-21 14:50:12 -0400

    Added visualization to the itterations to the console

--- AssignmentTwo.py
+++ AssignmentTwo.py
@@ -6,7 +6,6 @@ inSelection = True
 def PrintArray(arr):
     print("Array to be sorted: ", arr)  
 
-#This is a recursive function that does the merge sort algorithim to the array provided
 def MergeSort(arrToSort):
 
     #checks if the array is larger then one to sort
@@ -15,17 +14,26 @@ def MergeSort(arrToSort):
         leftData = arrToSort[:len(arrToSort)//2]#starts from the the start of the array to the midpoint
         rightData = arrToSort[len(arrToSort)//2:]#starts from the midpoint of the array to the end of the original array
 
+        #prints out the itterations of the mergesort algorithim
+        print("ITTERATION: ")
+        print("Array Being Split is: ", arrToSort)
+        print("Left Data Split: ", leftData, " Right Data Split: ", rightData)
+        print(" ")
+
         MergeSort(leftData)
         MergeSort(rightData)
-        
+
 
         #merge
         leftArrIndex = 0
         rightArrIndex = 0
         finalArrIndex = 0
 
+        #these set of while loops sort the data depending and insert them into the final array to be out putted
         while leftArrIndex < len(leftData) and rightArrIndex < len(rightData):
+            #checks if the leftside data is smaller then the right data at the index
             if leftData[leftArrIndex] < rightData[rightArrIndex]:
+                #inputs the value to the correct position in the array
                 arrToSort[finalArrIndex] = leftData[leftArrIndex]
                 leftArrIndex += 1
             else:
@@ -33,67 +41,38 @@ def MergeSort(arrToSort):
                 rightArrIndex += 1
 
             finalArrIndex += 1
-        
+
         while leftArrIndex < len(leftData):
             arrToSort[finalArrIndex] = leftData[leftArrIndex]
             leftArrIndex += 1
             finalArrIndex += 1
-        
+
         while rightArrIndex < len(rightData):
             arrToSort[finalArrIndex] = rightData[rightArrIndex]
             rightArrIndex += 1
             finalArrIndex += 1
 
-#partitions the array around the pivot element making the pivot the first element
-def partition(arrToSort, arrBegin, arrEnd):
-    pivot = arrToSort[arrBegin]
-    i = arrBegin + 1
-    j = arrEnd
-    
-    while True:
-        while i <= j and arrToSort[i] <= pivot:
-            i += 1
-        while i <= j and arrToSort[j] >= pivot:
-            j -= 1
-        if i <= j:
-            arrToSort[i], arrToSort[j] = arrToSort[j], arrToSort[i]
-        else:
-            break
-    
-    arrToSort[arrBegin], arrToSort[j] = arrToSort[j], arrToSort[arrBegin]
-    return j
+print("Welcome to Mergesort!!")
+print("Please input the size of the array you would like to sort(ex: input 10 to get an array size of 10 values)")
+arraySize = input()
+arraySize = int(arraySize)
+userArray = [0] * arraySize
+print("This is the new array before values inputted: ", userArray)
+for x in range(arraySize):
+    print("Please input a int value to be sorted")
+    arrayVal = input()
+    arrayVal = int(arrayVal)
+    userArray[x] = arrayVal
 
-#This is a recursive function that sorts the idArray
-def QuickSort(arrToSort, arrBegin, arrEnd):
-    if arrBegin < arrEnd:
-        pivot_index = partition(arrToSort, arrBegin, arrEnd)
-        QuickSort(arrToSort, arrBegin, pivot_index - 1)
-        QuickSort(arrToSort, pivot_index + 1, arrEnd)
+print(" ")
+print("This is the new array after values inputted: ", userArray)
 
-PrintArray(idArray)
+print("-------------------------------Performing Merge Sort --------------------------")
+MergeSort(userArray)
 
-while(inSelection):
-    print("How would you like to sort the array?(Input the name of the sort ex: MergeSort)")
-    print("1) Mergesort")
-    print("2) Quicksort")
-
-    userSelection = input()
-    print("Player Input: " + userSelection)
-
-    if userSelection.lower() == "mergesort" or userSelection.lower() == "quicksort":
-        inSelection = False
-    else:
-        print("ERROR: Please input your selection again")
-
-if userSelection.lower() == "mergesort":
-    print("Performing Merge sort!!!!!!")
-    MergeSort(idArray)
-elif userSelection.lower() == "quicksort":
-    print("Performing Quick sort!!!!")
-    QuickSort(idArray, 0, len(idArray) - 1)
 
 print("-------------------------------FINAL SORTED ARRAY --------------------------")
-print("Final sorted array: ", idArray)
+print("Final sorted array: ", userArray)
 
 
 
